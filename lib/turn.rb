b5619512e24bcac180e265cc08f65681cace0fc2
Stephen C Galvan <stephennoahgalvan@gmail.com>
2020-05-10 02:23:59 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -17,12 +17,12 @@ def valid_move?(board, index)
 end
 
 board = [" "," "," "," "," "," "," "," "," "]
-def position_taken?(board,index)
-  if board[index] == " " || board[index] == "" || board[index] == nil
-    return false
-  else
-    return true
-  end
+def position_taken?(board, index)
+   if (board[index] == " ") || (board[index] == "") || (board[index] == nil)
+      return false 
+   else
+      return true
+   end
 end
 
 board = [" "," "," "," "," "," "," "," "," "]
