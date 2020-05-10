76a92724c4806ff80494346c0a20657b4440c66b
Stephen C Galvan <stephennoahgalvan@gmail.com>
2020-05-10 02:45:12 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -38,7 +38,7 @@ def turn(board)
    index = input_to_index(user_input)
    if valid_move?(board, index)
      move(board, index, "X")
-     turn(board)
+   else turn(board)
    end
   display_board(board)
 end
