ea15c84cfb3b85e02f0bad22723ad0da45e2103e
Stephen C Galvan <stephennoahgalvan@gmail.com>
2020-05-10 02:24:02 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -19,7 +19,7 @@ end
 board = [" "," "," "," "," "," "," "," "," "]
 def position_taken?(board, index)
    if (board[index] == " ") || (board[index] == "") || (board[index] == nil)
-      return false 
+      return false
    else
       return true
    end
